2ad4fef29db6acfa0e0858954eb83641031ee351
Veronique <veroniqueauzias@gmail.com>
2020-11-20 08:12:07 +0100

    secure form and enter database

--- src/Model/JoinManager.php
+++ src/Model/JoinManager.php
@@ -4,5 +4,31 @@ namespace App\Model;
 
 class JoinManager extends AbstractManager
 {
+    /**
+     *
+     */
+   const TABLE = 'soldier';
 
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+    /**
+     * @param array $item
+     * @return int
+     */
+    public function insert(array $soldier): int
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `password`) VALUES (:name, :password)");
+        $statement->bindValue('name', $soldier['name'], \PDO::PARAM_STR);
+        $statement->bindValue('password', $soldier['password'], \PDO::PARAM_STR);
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
 }
